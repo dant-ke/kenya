c0c48aa79bcebac44360b1d148d327f87a447bf2
dant-ke <asmartchoize@gmail.com>
2017-11-04 13:54:39 +0300

    finished the Post model and Posts controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+Category.create(name: 'Our Expressions')
+Category.create(name: ' In Society')
+Category.create(name: 'Dinner is ready')
+Category.create(name: 'Curture')
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
